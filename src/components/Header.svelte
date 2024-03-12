daf37519388225f82b15a93b58c05fd7ab2e417e
Real Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2024-03-12 14:01:26 +0300

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -25,7 +25,11 @@
             <h1 class="font-semibold text-4xl md:text-5xl lg:text-5xl mt-20 md:mt-10 lg:mt-10">WhatsIP</h1>
             <div>
                 <input bind:value={ip} placeholder="Enter a IP" type="text" class="p-3 px-5 md:w-96 lg:w-96 rounded-2xl text-black mt-7">
+                {#if !ip}
+                <button disabled><i class="fa-solid fa-angle-right bg-black/10 p-4 px-5 rounded-2xl"></i></button>
+                {:else}
                 <button on:click={getIP}><i class="fa-solid fa-angle-right bg-black p-4 px-5 rounded-2xl"></i></button>
+                {/if}
                 <p class="mt-3 opacity-60 text-xs">No IP address is kept on the website.</p>
             </div>
         </div>
